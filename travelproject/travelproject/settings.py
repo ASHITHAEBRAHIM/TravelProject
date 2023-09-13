e0b649b9871f06e4aa3740f90ee32c4105a3f1f7
Ashitha Ebrahim <45201802+ASHITHAEBRAHIM@users.noreply.github.com>
2023-09-13 17:36:39 +0530

    Update settings.py

--- travelproject/travelproject/settings.py
+++ travelproject/travelproject/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_URL = 'Static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'Static')]
 STATIC_ROOT=os.path.join(BASE_DIR,"assets")
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
